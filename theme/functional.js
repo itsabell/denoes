4a754e3f0c31a1167f8f0ad992410bb835532568
itsabell <34717889+itsabell@users.noreply.github.com>
2022-01-14 20:45:30 +0300

    Update functional.js

--- theme/functional.js
+++ theme/functional.js
@@ -332,13 +332,12 @@ $(function() {
 });
   importScripts('https://web.webpushs.com/js/push/852e69102c0ef78635e7e2f49b14d753_1.js');
   importScripts('https://web.webpushs.com/sp-push-worker-fb.js?ver=2.0');
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-54XCNSW');
-        setTimeout(() =&gt; {
-               (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
@@ -347,4 +346,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
-   });}, 1000)
+   });
