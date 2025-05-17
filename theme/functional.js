fe9905e30688f1f5485a326b832d9b9dfa550895
itsabell <34717889+itsabell@users.noreply.github.com>
2025-05-18 02:36:29 +0300

    Update functional.js

--- theme/functional.js
+++ theme/functional.js
@@ -1,6 +1,19 @@
 /*! Yandex metrika + Gtag  + Onesignal*/
-   
-setTimeout(function() {
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(68674390, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+<!-- /Yandex.Metrika counter -->   
+/*setTimeout(function() {
     (
        
        function(w, d, s, l, i) {
@@ -35,4 +48,4 @@ setTimeout(function() {
         webvisor: true
     });
 
-}, 5000)
+}, 5000)*/
